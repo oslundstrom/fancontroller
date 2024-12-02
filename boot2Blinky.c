d258a0720757127ce9432a4ed14200dca37de5dc
Oskar Lundström <oslundstrom@gmail.com>
2024-12-02 15:19:41 +0100

    Change to GPIO 0

--- boot2Blinky.c
+++ boot2Blinky.c
@@ -3,7 +3,8 @@
 
 // Define necessary register addresses
 #define RESETS_RESET *(volatile uint32_t *) (0x4000c000)
-#define IO_BANK0_GPIO25_CTRL *(volatile uint32_t *) (0x400140cc)
+#define IO_BANK0_GPIO25_CTRL *(volatile uint32_t *) (0x40014000 + (25 * 8) + 4)
+#define IO_BANK0_GPIO0_CTRL *(volatile uint32_t *) (0x40014000 + (0 * 8) + 4)
 #define SIO_GPIO_OE_SET *(volatile uint32_t *) (0xd0000024)
 #define SIO_GPIO_OUT_XOR *(volatile uint32_t *) (0xd000001c)
 
@@ -13,18 +14,18 @@ __attribute__((section(".boot2"))) void bootStage2(void)
     // Bring IO_BANK0 out of reset state
     RESETS_RESET &= ~(1 << 5);
 
-    // Set GPIO 25 function to SIO
-    IO_BANK0_GPIO25_CTRL = 5;
+    // Set GPIO 0 function to SIO
+    IO_BANK0_GPIO0_CTRL = 5;
 
-    // Set output enable for GPIO 25 in SIO
-    SIO_GPIO_OE_SET |= 1 << 25;
+    // Set output enable for GPIO 0 in SIO
+    SIO_GPIO_OE_SET |= 1 << 0;
 
     while (true)
     {
         // Wait for some time
         for (uint32_t i = 0; i < 100000; ++i);
 
-        // Flip output for GPIO 25
-        SIO_GPIO_OUT_XOR |= 1 << 25;
+        // Flip output for GPIO 0
+        SIO_GPIO_OUT_XOR |= 1 << 0;
     }
 }
